4c02eba8f908a2c2b23378fd88b4a04d11d324df
Rexlin595 <142157321+Rexlin595@users.noreply.github.com>
2025-08-05 00:02:39 +0800

    new project

---
+++ src/AppTWO.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello from App2!')
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <p>This is a simple Vue.js application.</p>
+    <img alt="Vue logo" src="./assets/logo.svg" width="125" height="125" />
+  </div>
+</template>
+
+<style>
+h1 {
+  color: blue;
+}
+</style>
